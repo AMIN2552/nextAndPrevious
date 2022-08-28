13e8c0637ef0863c39902dee4561cfe6c6e41f50
user <amin7061205@gmail.com>
2022-08-28 17:10:24 +0300

    Commit

--- src/NextAndPrevious.java
+++ src/NextAndPrevious.java
@@ -8,8 +8,7 @@ public class NextAndPrevious {
          int b = a + 1;
          int c = a - 1;
         System.out.println("The next number for the number " + a + " is " + b + ".");
-        System.out.println("The next number for the number " + a + " is " + c + ".");
+        System.out.println("The previous number for the number " + a + " is " + c + ".");
 
     }
 }
-
